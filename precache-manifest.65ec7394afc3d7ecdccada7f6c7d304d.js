2a4181c3a72c6c0a64ccf852c0220f98667843d4
danielfct <d.pimenta@campus.fct.unl.pt>
2020-11-06 17:22:08 +0000

    Updates

---
+++ precache-manifest.65ec7394afc3d7ecdccada7f6c7d304d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2357a00cc8426c6f8b18391a23a84fa",
+    "url": "/manager-hub/index.html"
+  },
+  {
+    "revision": "a39a210262f4e82fcaca",
+    "url": "/manager-hub/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "39afa8117295305998f0",
+    "url": "/manager-hub/static/css/main.0b727d8a.chunk.css"
+  },
+  {
+    "revision": "a39a210262f4e82fcaca",
+    "url": "/manager-hub/static/js/2.473381d5.chunk.js"
+  },
+  {
+    "revision": "7583172986a2a3be83369da680ec3249",
+    "url": "/manager-hub/static/js/2.473381d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39afa8117295305998f0",
+    "url": "/manager-hub/static/js/main.dc3b46fc.chunk.js"
+  },
+  {
+    "revision": "39e0c30a5ef6c4764c59",
+    "url": "/manager-hub/static/js/runtime-main.6f39385b.js"
+  },
+  {
+    "revision": "747bba5b8d05c259d20fe3c8406b2e4a",
+    "url": "/manager-hub/static/media/logo.747bba5b.png"
+  }
+]);
