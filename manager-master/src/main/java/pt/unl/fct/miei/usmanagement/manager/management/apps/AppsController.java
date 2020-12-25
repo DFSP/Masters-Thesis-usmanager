b2ab9ad09d1291ed67bb53a7a43a60a08d1daed0
danielfct <d.pimenta@campus.fct.unl.pt>
2020-12-25 16:52:48 +0000

    Bug fix

--- manager-master/src/main/java/pt/unl/fct/miei/usmanagement/manager/management/apps/AppsController.java
+++ manager-master/src/main/java/pt/unl/fct/miei/usmanagement/manager/management/apps/AppsController.java
@@ -24,7 +24,6 @@
 
 package pt.unl.fct.miei.usmanagement.manager.management.apps;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
